65231df0b9eb541c6d44aa9d882f5152c932b7df
Francisco Gonzalez <francisco.gonzalez@piksel.com>
2019-06-19 18:46:18 +0200

    PROCESSING-3926 - Implement credential-per-owner in HttpClientAccessCredentialProvider

--- src/main/java/com/piksel/sequoia/clientsdk/token/HttpClientAccessCredentialProvider.java
+++ src/main/java/com/piksel/sequoia/clientsdk/token/HttpClientAccessCredentialProvider.java
@@ -20,14 +20,6 @@ package com.piksel.sequoia.clientsdk.token;
  * #L%
  */
 
-import static com.google.api.client.auth.oauth2.BearerToken.authorizationHeaderAccessMethod;
-
-import java.io.IOException;
-import java.net.URL;
-import java.util.HashMap;
-
-import javax.inject.Inject;
-
 import com.google.api.client.auth.oauth2.ClientCredentialsTokenRequest;
 import com.google.api.client.auth.oauth2.Credential;
 import com.google.api.client.auth.oauth2.TokenResponse;
@@ -42,14 +34,26 @@ import com.google.api.client.http.HttpTransport;
 import com.google.api.client.http.HttpUnsuccessfulResponseHandler;
 import com.google.api.client.http.UrlEncodedContent;
 import com.google.api.client.json.JsonFactory;
+import com.google.common.collect.Maps;
 import com.piksel.sequoia.annotations.Internal;
 import com.piksel.sequoia.clientsdk.MessageConfiguration;
 import com.piksel.sequoia.clientsdk.PreconfiguredHostRegistry;
 import com.piksel.sequoia.clientsdk.configuration.ComponentCredentials;
 import com.piksel.sequoia.clientsdk.configuration.HostConfiguration;
 import com.piksel.sequoia.clientsdk.recovery.RequestRecoveryStrategyProvider;
-
+import com.piksel.sequoia.clientsdk.registry.RegisteredService;
+import com.piksel.sequoia.clientsdk.registry.RegistryClient;
+import com.piksel.sequoia.clientsdk.resource.ResourceIterable;
 import lombok.extern.slf4j.Slf4j;
+
+import javax.inject.Inject;
+import java.io.IOException;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+import static com.google.api.client.auth.oauth2.BearerToken.authorizationHeaderAccessMethod;
 
 /**
  * An {@link DataServicesCredentialProvider} that communicates with the data
@@ -71,20 +75,25 @@ public class HttpClientAccessCredentialProvider
     private final RequestRecoveryStrategyProvider requestRecoveryStrategyProvider;
     private final MessageConfiguration messageConfiguration;
     private final ClientGrantCredentialUnsuccessfulResponseHandler unsuccessfulResponseHandler;
+    private final RegistryClient registryClient;
 
     private ClientGrantCredential credential;
     private boolean allowCredentialRefresh = true;
+
+    private Map<String, ClientGrantCredential> credentialsMap = Maps.newConcurrentMap();
 
     @Inject
     public HttpClientAccessCredentialProvider(
             PreconfiguredHostRegistry hostRegistry,
             RequestRecoveryStrategyProvider requestRecoveryStrategyProvider,
             MessageConfiguration transportConfiguration,
-            ClientGrantCredentialUnsuccessfulResponseHandler unsuccessfulResponseHandler) {
+            ClientGrantCredentialUnsuccessfulResponseHandler unsuccessfulResponseHandler,
+            RegistryClient registryClient) {
         this.hostRegistry = hostRegistry;
         this.requestRecoveryStrategyProvider = requestRecoveryStrategyProvider;
         this.messageConfiguration = transportConfiguration;
         this.unsuccessfulResponseHandler = unsuccessfulResponseHandler;
+        this.registryClient = registryClient;
         log.debug("HttpClientAccessCredentialProvider constructed");
     }
 
@@ -114,9 +123,45 @@ public class HttpClientAccessCredentialProvider
     }
 
     @Override
+    public Credential getCredential(String owner) {
+        ClientGrantCredential credential = credentialsMap.get(owner);
+        if (credential == null && allowCredentialRefresh) {
+            log.debug("No client credential stored - requesting a new one");
+            TokenResponse response = executeTokenRequest(owner);
+
+            credential = ClientGrantCredential.builder()
+                    .unsuccessfulResponseHandler(unsuccessfulResponseHandler)
+                    .credentialBuilder(new ClientGrantCredential.Builder(
+                            authorizationHeaderAccessMethod())
+                            .setTransport(
+                                    messageConfiguration.getTransport())
+                            .setJsonFactory(messageConfiguration
+                                    .getJsonFactory())
+                            .setTokenServerUrl(
+                                    buildIdentityAccessTokenUrl(owner))
+                            .setClientAuthentication(
+                                    createBasicAuthHeader()))
+                    .build();
+            log.debug("Built client credential [{}]", credential);
+            credential.setFromTokenResponse(response);
+            credentialsMap.put(owner, credential);
+        }
+        return credential;
+    }
+
+    @Override
     public void revokeToken() {
         if (credential != null) {
             executeRevokeRequest();
+            credential.disableRefreshToken();
+        }
+        allowCredentialRefresh = false;
+    }
+
+    @Override
+    public void revokeToken(String owner) {
+        if (credential != null) {
+            executeRevokeRequest(owner);
             credential.disableRefreshToken();
         }
         allowCredentialRefresh = false;
@@ -135,10 +180,28 @@ public class HttpClientAccessCredentialProvider
         return callDataServices(tokenServerUrl, jsonFactory, transport);
     }
 
+    private TokenResponse executeTokenRequest(String owner) {
+        log.debug("Executing a token request");
+        GenericUrl tokenServerUrl = buildIdentityAccessTokenUrl(owner);
+        JsonFactory jsonFactory = messageConfiguration.getJsonFactory();
+        HttpTransport transport = messageConfiguration.getTransport();
+        return callDataServices(tokenServerUrl, jsonFactory, transport);
+    }
+
     private void executeRevokeRequest() {
         try {
             log.debug("Executing a revoke request");
             aRequestFactory().buildPostRequest(buildIdentityRevokeTokenUrl(),
+                    buildRevokeContent()).execute();
+        } catch (IOException e) {
+            throw anUnexpectedTokenRevokeException(e);
+        }
+    }
+
+    private void executeRevokeRequest(String owner) {
+        try {
+            log.debug("Executing a revoke request");
+            aRequestFactory().buildPostRequest(buildIdentityRevokeTokenUrl(owner),
                     buildRevokeContent()).execute();
         } catch (IOException e) {
             throw anUnexpectedTokenRevokeException(e);
@@ -199,12 +262,39 @@ public class HttpClientAccessCredentialProvider
         return resolveHostUrlWithPath(OAUTH_REVOKE_URL_PATH);
     }
 
+    private GenericUrl buildIdentityAccessTokenUrl(String owner) {
+        return resolveHostUrlForOwner(owner);
+    }
+
+    private GenericUrl buildIdentityRevokeTokenUrl(String owner) {
+        return resolveHostUrlForOwner(owner);
+    }
+
     private GenericUrl resolveHostUrlWithPath(String path) {
         HostConfiguration hostConfiguration = hostRegistry.identity();
         URL resolvedUri = hostConfiguration.getBaseUrl();
         GenericUrl genericUrl = new GenericUrl(resolvedUri);
         genericUrl.appendRawPath(OAUTH_TOKEN_URL_PATH);
         return genericUrl;
+    }
+
+    private GenericUrl resolveHostUrlForOwner(String owner) {
+        HostConfiguration hostConfiguration = getIdentityHostFromRegistry(owner).orElse(hostRegistry.identity());
+        URL resolvedUri = hostConfiguration.getBaseUrl();
+        GenericUrl genericUrl = new GenericUrl(resolvedUri);
+        genericUrl.appendRawPath(OAUTH_TOKEN_URL_PATH);
+        return genericUrl;
+    }
+
+    private Optional<HostConfiguration> getIdentityHostFromRegistry(String owner) {
+        ResourceIterable<RegisteredService> serviceResourceIterable =registryClient.getServiceRegistryListForOwner(owner);
+        while(serviceResourceIterable.hasNext()) {
+            RegisteredService registeredService = serviceResourceIterable.next();
+            if(registeredService.getName().equals("identity")) {
+                return Optional.of(new HostConfiguration(registeredService.getLocation()));
+            }
+        }
+        return Optional.empty();
     }
 
     private HttpRequestFactory aRequestFactory() {
